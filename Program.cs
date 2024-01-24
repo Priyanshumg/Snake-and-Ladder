307ce3460f44f99ff922fb6a29d51aef8323fd40
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-01-24 10:48:01 +0530

    [priyanshu] Created a method to roll dice

--- Program.cs
+++ Program.cs
@@ -10,12 +10,23 @@ namespace Snake_and_Ladder
 {
     internal class Program
     {
+        // UC 1
         private int position = 0;
+
+        static int dice()
+        {
+            // UC 2
+            Random random = new Random();
+            return random.Next(0, 7);
+        }
         static void Main(string[] args)
         {
             Program program = new Program();
             int Player1 = program.position = 10;
             Console.WriteLine(Player1);
+            Console.Write("Rolling Dice ... ");
+            Console.WriteLine(dice());
+            Console.ReadLine();
         }
     }
 }
