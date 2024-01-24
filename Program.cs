ea7fc130df0f36a0069bbe59205ab94651cb572f
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-01-24 10:33:24 +0530

    [Priyanshu] Player Position Set

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,12 @@ namespace Snake_and_Ladder
 {
     internal class Program
     {
+        private int position = 0;
         static void Main(string[] args)
         {
-
+            Program program = new Program();
+            int Player1 = program.position = 10;
+            Console.WriteLine(Player1);
         }
     }
 }
