95d0a1f768e992a9315aa6a5955d0f5875d88508
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-01-24 19:33:01 +0530

    [Priyanshu] Test: Tested switch cases

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -13,19 +14,45 @@ namespace Snake_and_Ladder
         // UC 1
         private int position = 0;
 
-        static int dice()
+        static int rand()
         {
             // UC 2
             Random random = new Random();
             return random.Next(0, 7);
         }
+
+        static int cases()
+        {
+            Random cases = new Random();
+            return cases.Next(0, 4);
+        }
+
+
         static void Main(string[] args)
         {
+            int dice = rand();
             Program program = new Program();
             int Player1 = program.position = 10;
             Console.WriteLine(Player1);
-            Console.Write("Rolling Dice ... ");
-            Console.WriteLine(dice());
+            int switch_case = cases();
+            switch (switch_case)
+            {
+                case 1:
+                {
+                    Console.WriteLine("This is Case 1");
+                    break;
+                }
+                case 2:
+                {
+                    Console.WriteLine("This is Case 2");
+                    break;
+                }
+                case 3:
+                {
+                    Console.WriteLine("This is Case 3");
+                    break;
+                }
+            }
             Console.ReadLine();
         }
     }
